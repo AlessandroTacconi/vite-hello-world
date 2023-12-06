0b44caf7c8ad5b41c08c014841b0384ceca498b4
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-12-06 16:45:00 +0100

    feat: creazione-titolo-app

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+      titolo: 'La mia prima app con Vite!',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ titolo }}</h1>
+</template>
